b53240bec2f3c906c86d9f2c6c149f3e3f63b409
brutchley <bhutchison0901@yahoo.com>
2024-01-07 16:38:13 -0600

    FINALLY have working webpage with no styling, but handlebars template showing up! Links work, no errors yet. Will still need to keep working on this to improve user interface

--- server.js
+++ server.js
@@ -14,8 +14,23 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
-
 const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+
+/*const sess = {
     secret: process.env.SESSION_SECRET,
     cookie: {
       maxAge: 300000,
@@ -28,7 +43,7 @@ const sess = {
     store: new SequelizeStore({
       db: sequelize
     })
-};
+};*/
 
 app.use(session(sess));
 
