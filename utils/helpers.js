dc91742b59b2034b7343b0a2e1549687857c9c89
paloma wells <palomawells@palomas-macbook.lan>
2024-01-08 20:30:26 -0600

    test

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,25 +1,16 @@
 module.exports = {
     format_date: (date) => {
-<<<<<<< HEAD
-      return date.toLocaleDateString();
-    },
-    format_amount: (amount) => {
-=======
       // Format date as MM/DD/YYYY
       return date.toLocaleDateString();
     },
     format_amount: (amount) => {
       // format large numbers with commas
->>>>>>> 3b50e45145934b1b7f4704f919d0baee5b8d6f98
       return parseInt(amount).toLocaleString();
     },
     get_emoji: () => {
       const randomNum = Math.random();
   
-<<<<<<< HEAD
-=======
       // Return a random emoji
->>>>>>> 3b50e45145934b1b7f4704f919d0baee5b8d6f98
       if (randomNum > 0.7) {
         return `<span for="img" aria-label="lightbulb">💡</span>`;
       } else if (randomNum > 0.4) {
@@ -28,9 +19,4 @@ module.exports = {
         return `<span for="img" aria-label="gear">⚙️</span>`;
       }
     },
-<<<<<<< HEAD
-  };
-  
-=======
 };
->>>>>>> 3b50e45145934b1b7f4704f919d0baee5b8d6f98
